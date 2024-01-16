a3741af58732caacd542b4fa7d401bcfbba4eaaf
Leonardo Santos <leonardojosantos@gmail.com>
2024-01-16 15:27:26 -0300

    Class 241 - Creating the first routes with BrowserRouter, Routes and Route

--- tutorial-react-router-dom/src/main.tsx
+++ tutorial-react-router-dom/src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './components/Home';
+import { About } from './components/About';
 import './styles/global.css'
-import { Home } from './components/Home'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
