a2b55d3f56c832b09ad5d9734d68993f151fc277
Leonardo Santos <leonardojosantos@gmail.com>
2024-01-16 19:14:43 -0300

    Class 245 - Route 404: Route takes everything that doesn't exist

--- tutorial-react-router-dom/src/main.tsx
+++ tutorial-react-router-dom/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Redirect } from './components/Redirect';
+import { NotFound } from './components/NotFound';
 import { About } from './components/About';
 import { Home } from './components/Home';
 import { Menu } from './components/Menu';
@@ -18,6 +19,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Route path='/posts/:id' element={<Post />} />
         <Route path='/posts' element={<Post />} />
         <Route path='/redirect' element={<Redirect />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
