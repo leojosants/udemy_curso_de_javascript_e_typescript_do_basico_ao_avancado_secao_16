a82e3df6feba6c1744efe79f6e8c5f2764c66e4b
Leonardo Santos <leonardojosantos@gmail.com>
2024-01-16 18:32:31 -0300

    Class 243 - useParams and useSearchParams: Using URL and Query String parameters

--- tutorial-react-router-dom/src/main.tsx
+++ tutorial-react-router-dom/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './components/Home';
 import { About } from './components/About';
 import { Menu } from './components/Menu';
+import { Post } from './components/Post';
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -13,6 +14,8 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path='/posts/:id' element={<Post />} />
+        <Route path='/posts' element={<Post />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
